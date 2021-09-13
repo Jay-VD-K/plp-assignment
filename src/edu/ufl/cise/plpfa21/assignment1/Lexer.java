e353a70e87c637f9154e5dafce2b8e83ee234d93
Jay-VD-K <jayavidhi.k@gmail.com>
2021-09-13 15:30:07 -0400

    added regex check for lexer and tokens

--- src/edu/ufl/cise/plpfa21/assignment1/Lexer.java
+++ src/edu/ufl/cise/plpfa21/assignment1/Lexer.java
@@ -1,42 +1,102 @@
 package edu.ufl.cise.plpfa21.assignment1;
 
-public class Lexer implements IPLPLexer{
-
+public class Lexer implements IPLPLexer {
 	public String input;
 	public String parentInput;
 	
 	public Lexer(String input) {
-		this.input= input;
-		this.parentInput= input;
-	}
-	
-	public String next() {
-		int i=0; String s="";
-		
-		if(input.charAt(0)==' ') {
-			while(input.charAt(i)==' ') {
-				s=s+input.charAt(i);
-				i++;
-			}
-		}
-		
-		for(;i<input.length();i++) {
-			if((int)input.charAt(i)==10 || input.charAt(i)==' ')
-				{
-					input=input.substring(i+1);
-					return s;
-					
-				}
-			else
-				s=s+input.charAt(i);
-		}
-		return s;
+		this.input = input;
+		this.parentInput = input;
 	}
 
 	@Override
 	public IPLPToken nextToken() throws LexicalException {
-		String s= next();
+		String s = "", sample = "int i = i+7;";
+		int flag = 0, i = 0;
+		if (input.charAt(0) == '\n' || (input.charAt(0) == '\r' && input.charAt(1) == '\n')) {
+			s += input.charAt(0);
+			return new Tokens(s, parentInput);
+		} else if (input.charAt(0) == ' ') {
+			while (input.charAt(i) == ' ') {
+				// s=s+input.charAt(i);
+				i++;
+			}
+		}
+
+		for (; i < input.length()-1; i++) {
+			// if regex input break; s=token; break;else sthen go to switch
+			switch (input.charAt(i)) {
+			case ' ':
+			case '\n':
+				// case \r\n:
+			case '\t': {
+				input = input.substring(i + 1);
+				flag = 1;
+			}
+				break;
+			case '&': {
+				s += input.charAt(i);
+				if (input.charAt(i + 1) == '&') {
+					s += input.charAt(i + 1);
+					input = input.substring(i + 2);
+					return new Tokens(s, parentInput);
+				}
+				input = input.substring(i + 1);
+				return new Tokens(s, parentInput);
+			}
+			case '|': {
+				s += input.charAt(i);
+				if (input.charAt(i + 1) == '|') {
+					s += input.charAt(i + 1);
+					input = input.substring(i + 2);
+					return new Tokens(s, parentInput);
+				}
+				input = input.substring(i + 1);
+				return new Tokens(s, parentInput);
+			}
+			case '=': {
+				s += input.charAt(i);
+				if (input.charAt(i + 1) == '=') {
+					s += input.charAt(i + 1);
+					input = input.substring(i + 2);
+					return new Tokens(s, parentInput);
+				}
+				input = input.substring(i + 1);
+				return new Tokens(s, parentInput);
+			}
+			case '!': {
+				s += input.charAt(i);
+				if (input.charAt(i + 1) == '=') {
+					s += input.charAt(i + 1);
+					input = input.substring(i + 2);
+					return new Tokens(s, parentInput);
+				}
+				input = input.substring(i + 1);
+				return new Tokens(s, parentInput);
+			}
+			case '*', '/', '+', '-', ';', ',', ':', '(', ')', '[', ']', '<', '>': {
+				s += input.charAt(i);
+				input = input.substring(i + 1);
+				return new Tokens(s, parentInput);
+			}
+			case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9': {
+				s += input.charAt(i);
+				if (((int) input.charAt(i + 1) >= 97 && (int) input.charAt(i + 1) <= 122)
+						|| ((int) input.charAt(i + 1) >= 65 && (int) input.charAt(i + 1) <= 90)) {
+					input = input.substring(i + 1);
+					return new Tokens(s, parentInput);
+				}
+				break;
+			}
+			default:
+				s += input.charAt(i);
+				break;
+			}
+			if (flag == 1) {
+				break;
+			}
+		}
+		System.out.println(s);
 		return new Tokens(s, parentInput);
 	}
-
 }
--- src/edu/ufl/cise/plpfa21/assignment1/Tokens.java
+++ src/edu/ufl/cise/plpfa21/assignment1/Tokens.java
@@ -3,49 +3,191 @@ package edu.ufl.cise.plpfa21.assignment1;
 public class Tokens implements IPLPToken {
 
 	String input, parentInput;
+
 	public Tokens(String input, String parentInput) {
-		this.input= input;
-		this.parentInput= parentInput;
+		this.input = input;
+		this.parentInput = parentInput;
 	}
 
 	@Override
 	public Kind getKind() {
-		if(parentInput == """
+		// TODO Auto-generated method stub
+		String s = input;
+		Character c = s.charAt(0);
+		String start = c.toString();
+		String remain = s.substring(1, s.length());
 
-				""")
-			return Kind.EOF;
-		
-		else
+		if (start.matches("[a-zA-Z_$]") && remain.matches("[a-zA-Z_$0-9]+")) {
+
+			switch (s) {
+			case "var":
+				return Kind.KW_VAR;
+
+			case "val":
+				return Kind.KW_VAL;
+
+			case "fun":
+				return Kind.KW_FUN;
+
+			case "do":
+				return Kind.KW_DO;
+
+			case "end":
+				return Kind.KW_END;
+
+			case "let":
+				return Kind.KW_LET;
+
+			case "switch":
+				return Kind.KW_SWITCH;
+
+			case "case":
+				return Kind.KW_CASE;
+
+			case "default":
+				return Kind.KW_DEFAULT;
+
+			case "if":
+				return Kind.KW_IF;
+
+			case "else":
+				return Kind.KW_ELSE;
+
+			case "while":
+				return Kind.KW_WHILE;
+
+			case "return":
+				return Kind.KW_RETURN;
+
+			case "nil":
+				return Kind.KW_NIL;
+
+			case "true":
+				return Kind.KW_TRUE;
+
+			case "false":
+				return Kind.KW_FALSE;
+
+			case "int":
+				return Kind.KW_INT;
+
+			case "String":
+				return Kind.KW_STRING;
+
+			case "boolean":
+				return Kind.KW_BOOLEAN;
+
+			case "list":
+				return Kind.KW_LIST;
+
+			case "float":
+				return Kind.KW_FLOAT;
+
+			default:
+				return Kind.IDENTIFIER;
+
+			}
+		} else if (s.matches("[0-9]+")) {
+			return Kind.INT_LITERAL;
+
+		} else if (start.matches("[a-zA-Z_$]"))
 			return Kind.IDENTIFIER;
+		else {
+			switch (s) {
+			case "=":
+				return Kind.ASSIGN;
+
+			case ",":
+				return Kind.COMMA;
+
+			case ";":
+				return Kind.SEMI;
+
+			case ":":
+				return Kind.COLON;
+
+			case "(":
+				return Kind.LPAREN;
+
+			case ")":
+				return Kind.RPAREN;
+
+			case "[":
+				return Kind.LSQUARE;
+
+			case "]":
+				return Kind.RSQUARE;
+
+			case "&&":
+				return Kind.AND;
+
+			case "||":
+				return Kind.OR;
+
+			case "<":
+				return Kind.LT;
+
+			case ">":
+				return Kind.GT;
+
+			case "==":
+				return Kind.EQUALS;
+
+			case "!=":
+				return Kind.NOT_EQUALS;
+
+			case "!":
+				return Kind.BANG;
+
+			case "+":
+				return Kind.PLUS;
+
+			case "-":
+				return Kind.MINUS;
+
+			case "*":
+				return Kind.TIMES;
+
+			case "/":
+				return Kind.DIV;
+
+			case "\n":
+				return Kind.EOF;
+
+			default:
+				return Kind.ERROR;
+
+			}
+		}
 	}
 
 	@Override
 	public String getText() {
-		String s="";
-		for(int i=0;i<input.length();i++)
-			if(input.charAt(i)!=' ')
-				s=s+input.charAt(i);
+		String s = "";
+		for (int i = 0; i < input.length(); i++)
+			if (input.charAt(i) != ' ')
+				s = s + input.charAt(i);
 		return s;
 	}
 
 	@Override
 	public int getLine() {
 		int l = parentInput.indexOf(input);
-		int count=1;
-		for(int i=0;i<l;i++) {
-			if((int)parentInput.charAt(i)==10)
+		int count = 1;
+		for (int i = 0; i < l; i++) {
+			if ((int) parentInput.charAt(i) == 10)
 				count++;
 		}
 		return count;
-			
+
 	}
 
 	@Override
 	public int getCharPositionInLine() {
-		for(int i=0;i<input.length();i++)
-			if(input.charAt(i)!=' ')
+		for (int i = 0; i < input.length(); i++)
+			if (input.charAt(i) != ' ')
 				return i;
-		return input.length()-1;
+		return input.length() - 1;
 	}
 
 	@Override
