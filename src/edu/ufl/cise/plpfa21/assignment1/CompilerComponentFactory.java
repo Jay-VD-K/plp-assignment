eace1066f062ab9f904649271ec85701919e7309
Jay-VD-K <jayavidhi.k@gmail.com>
2021-11-30 15:39:52 -0500

    assign5 setup

--- src/edu/ufl/cise/plpfa21/assignment1/CompilerComponentFactory.java
+++ src/edu/ufl/cise/plpfa21/assignment1/CompilerComponentFactory.java
@@ -4,6 +4,7 @@ import edu.ufl.cise.plpfa21.assignment2.IPLPParser;
 import edu.ufl.cise.plpfa21.assignment2.Parser;
 import edu.ufl.cise.plpfa21.assignment3.ast.ASTVisitor;
 import edu.ufl.cise.plpfa21.assignment4.TypeCheckVisitor;
+import edu.ufl.cise.plpfa21.assignment5.StarterCodeGenVisitor;
 
 public class CompilerComponentFactory {
 
@@ -20,6 +21,11 @@ public class CompilerComponentFactory {
 	public static ASTVisitor getTypeCheckVisitor() {
 		// Replace this with whatever is needed for your compiler
 		return new TypeCheckVisitor();
+	}
+	
+	public static ASTVisitor getCodeGenVisitor(String className, String packageName, String sourceFileName) {
+		//Replace this with whatever is needed for your compiler
+		return new StarterCodeGenVisitor(className,packageName, sourceFileName);
 	}
 
 }
